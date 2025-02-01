d6fccaadd724a0919c533259e6a23bcf8666d49a
Lukas Schwab <lukas.schwab@gmail.com>
2025-01-31 17:59:01 -0800

    Simplify test names

--- widget_test.go
+++ widget_test.go
@@ -12,19 +12,19 @@ import (
 	"github.com/peterldowns/testy/assert"
 )
 
-func TestWidgetID_Marshal(t *testing.T) {
+func TestID_Marshal(t *testing.T) {
 	bytes, err := json.Marshal(widget.NilID)
 	assert.Nil(t, err)
 	assert.Equal(t, "\"00000000-0000-0000-0000-000000000000\"", string(bytes))
 }
 
-func TestWidgetID_Unmarshal(t *testing.T) {
+func TestID_Unmarshal(t *testing.T) {
 	widgetID := new(widget.ID)
 	err := json.Unmarshal([]byte("\"00000000-0000-0000-0000-000000000000\""), widgetID)
 	assert.Nil(t, err)
 	assert.Equal(t, widget.NilID, *widgetID)
 }
 
-func TestWidgetID_String(t *testing.T) {
+func TestID_String(t *testing.T) {
 	assert.Equal(t, "00000000-0000-0000-0000-000000000000", fmt.Sprint(widget.NilID))
 }
